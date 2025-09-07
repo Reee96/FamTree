418198e22bba73fe4e6527b18b406d7b8de8179e
Rikuro <111038443+Rikuro-Ikehata@users.noreply.github.com>
2025-09-07 12:09:58 +0900

    Initial Commit

---
+++ FamTreeTests/FamTreeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FamTreeTests.swift
+//  FamTreeTests
+//
+//  Created by Rikuro Ikehata on 2025/09/07.
+//
+
+import Testing
+@testable import FamTree
+
+struct FamTreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
